826b6c32fbbf47b98b98c969fd3b0636d1eb56ef
SanderPs <dev@sanderpeschier.nl>
2024-07-16 13:27:24 +0200

    update commander options

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,28 @@ import * as x from 'wmu';
 
 const chalk = require('chalk');
 const path = require('path');
-const program = require('commander');
-
-console.log(
-	chalk.red('wmu-cli')
-  );
+//const program = require('commander');
+import program from 'commander';
 
 program
-  .version('0.0.1')
+  .name("wmu")
+  .usage("[global options] folder | file")
+  .version('0.0.1', '-v, --vers', 'output the current version')
   .description("A CLI for WMU (Writers-Mark-Up)")
   .option('-f, --folder <loc>', 'Path to convert', '.')
-  .option('-p, --page', 'Output full html (include html and body tags)')
+  .option('-p, --page', 'Output full html (include html and body tags)', 'page')
   .parse(process.argv);
 
-  if (program.page) {
+console.log(
+  chalk.red(program.help())
+);
 
-  }
-  
-  console.log(`Folder: ${program.folder}`);
-  
-  console.log(x.transformFragment("hi **there**",{}));
+let opt = {} as x.IConfig;
+if (program.page) {
+  opt.format = 'page'
+}
+
+console.log(`Folder: ${program.folder}`);
+
+console.log(x.transformFragment("hi **there**", {}));
+
